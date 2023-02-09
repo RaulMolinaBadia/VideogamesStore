0e8c60c468c138dc6b19f297390907209e9cf903
Raul Molina <rmolina2003.rm@gmail.com>
2023-02-09 16:42:04 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
+    <title>VideoGames</title>
+</head>
+
+<body>
+    <p><a href="{{ url('/loginForm') }}">Videogames</a></p>
+    <p><a href="{{ url('/loginForm') }}">Categories</a></p>
+</body>
+
+</html>
